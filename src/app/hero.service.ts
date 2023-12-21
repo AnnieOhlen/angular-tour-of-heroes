b12f123a9557d2a78a3ab70b0ca3ae56bb5dc6e3
Annie <115146344+AnnieOhlen@users.noreply.github.com>
2023-12-21 14:41:19 +0100

    Message & hero components. Hero service. Small changes.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,11 +11,12 @@ export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
   constructor(private messageService: MessageService) {
-    
+
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -4,6 +4,7 @@ import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -27,9 +28,11 @@ export class HeroesComponent {
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
-  constructor(private heroService: HeroService) {}
+  constructor(private heroService: HeroService,
+    private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
